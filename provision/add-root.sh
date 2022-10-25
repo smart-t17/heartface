2b5cd23459611eef8cc314eaaeda8f04a485baae
Tamara Eaton <teatoncoding@gmail.com>
2022-10-24 19:10:10 -0700

    remove provisions

--- provision/add-root.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Add current user's ssh public key to the authorized keys
-#  of root on the target system.
-#
-
-cat $HOME/.ssh/id_rsa.pub | ssh root@$1 -C "mkdir -p /root/.ssh && cat >> /root/.ssh/authorized_keys2 && chmod 700 /root/.ssh && chmod 600 /root/.ssh/authorized_keys2"
--- provision/nginx-with-upload-module/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker build -t nginx-upload .
-
-cp /usr/sbin/nginx /usr/sbin/nginx.orig
-docker run -t -v /usr/sbin:/dest nginx-upload bash -c 'cp /usr/sbin/nginx /usr'
